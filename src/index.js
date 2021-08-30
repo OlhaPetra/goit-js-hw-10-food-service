70d7e00b427c0e6725cb6652dc25f76b66215cb1
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-30 22:38:13 +0300

    add localStorage

--- src/index.js
+++ src/index.js
@@ -10,19 +10,24 @@ menuItems.insertAdjacentHTML('beforeend', cardDishTpl(menu));
 const bodyTheme = document.querySelector('body');
 const switchToggle = document.querySelector('#theme-switch-toggle');
 
+
 bodyTheme.classList.add('light-theme');
 
 switchToggle.addEventListener('change', onCheckbox);
+
+body.classList.add(localStorage.getItem('currentTheme'));
+
 
 function onCheckbox(event) {
     event.preventDefault();
     if (this.checked) {
         bodyTheme.classList.add('dark-theme');
         bodyTheme.classList.remove('light-theme');
+        localStorage.setItem('currentTheme', 'dark-theme')
+        
     } else {
         bodyTheme.classList.add('light-theme');
-        bodyTheme.classList.remove('dark-theme');        
+        bodyTheme.classList.remove('dark-theme');
+        localStorage.setItem('currentTheme', 'light-theme')
     }
 }
-
-/* LocalStorage */
